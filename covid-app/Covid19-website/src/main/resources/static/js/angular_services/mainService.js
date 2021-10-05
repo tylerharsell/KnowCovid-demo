d5b39fdd65d7468fa8d7a79e19561d2b10abb9b0
wangso <wangso@missouri.edu>
2021-10-05 09:16:41 -0500

    change to Jenkins ports

--- covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
+++ covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
@@ -1,7 +1,7 @@
 app.service('mainService', function($http, $q, $location, $window) {
 
 	//local
-    var serverURL = "http://54.196.221.152:8080/Covid-19/";
+    var serverURL = "http://54.196.221.152:9090/Covid-19/";
 
     //server
     //var serverURL = "http://35.223.93.141:8080/KnowCOVID19/";
