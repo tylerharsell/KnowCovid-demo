15890ad55193579b1ba81144ba2fd8602bfbd1d5
wangso <wangso@missouri.edu>
2022-02-02 17:11:56 -0600

    update

--- covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
+++ covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
@@ -3,10 +3,6 @@ app.service('mainService', function($http, $q, $location, $window) {
 	//local
     //var serverURL = "http://localhost:9300/Covid-19/";
     var serverURL = "http://54.82.20.123:9090/Covid-19/";
-
-    //server
-    //var serverURL = "http://35.223.93.141:8080/KnowCOVID19/";
-
     
 	return {
 	   callPostRestAPI : function(url, data) {
