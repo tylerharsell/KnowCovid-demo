0c22136360111928037ed28061a178029a21ad09
wangso <wangso@missouri.edu>
2022-04-08 14:56:56 -0500

    Update mainService.js

--- covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
+++ covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
@@ -2,7 +2,7 @@ app.service('mainService', function($http, $q, $location, $window) {
 
 	//local
     //var serverURL = "http://localhost:9300/Covid-19/";
-    var serverURL = "http://34.207.68.65:9090/Covid-19/";
+    var serverURL = "http://Your_Server_IP:9090/Covid-19/";
     
 	return {
 	   callPostRestAPI : function(url, data) {
