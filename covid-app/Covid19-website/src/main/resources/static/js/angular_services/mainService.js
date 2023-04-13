8e16237138448941bf5eb0e8127c9d0e21430ec8
Tyler Harsell <33333678+tylerharsell@users.noreply.github.com>
2023-04-13 09:35:05 -0500

    Update mainService.js

--- covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
+++ covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
@@ -2,7 +2,7 @@ app.service('mainService', function($http, $q, $location, $window) {
 
 	//local
     //var serverURL = "http://localhost:9300/Covid-19/";
-    var serverURL = "http://Server_IP:9090/Covid-19/";
+    var serverURL = "http://52.23.210.129:9090/Covid-19/";
     
 	return {
 	   callPostRestAPI : function(url, data) {
